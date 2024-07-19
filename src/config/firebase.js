e8738e53e14666c98f79750a9f06b8ce4674c6d1
Impactmindz <impactmindz@gmail.com>
2024-07-19 18:31:19 +0530

    update

---
+++ src/config/firebase.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFlMajT4Y91h_FbpgLS_tIDzwqfM6_0Vk",
+  authDomain: "onlineordering-3025e.firebaseapp.com",
+  projectId: "onlineordering-3025e",
+  storageBucket: "onlineordering-3025e.appspot.com",
+  messagingSenderId: "708945464297",
+  appId: "1:708945464297:web:013ad425143ac741d75978",
+  measurementId: "G-NG3PNKQLT5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
